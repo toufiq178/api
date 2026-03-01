692aceb8ed5e208838955c61d7d8527e625f969d
toufiq178 <tajahmedtoufiq1@gmail.com>
2026-03-01 21:29:32 +0600

    task & fetch

--- JSON.js
+++ JSON.js
@@ -6,13 +6,27 @@ const person = {
     isMarried : false,
 }
 
-console.log( person, typeof person);
+// console.log( person, typeof person);
 
 // object to convert JSON 
 const personJson = JSON.stringify(person);
-console.log(personJson , typeof personJson);
+// console.log(personJson , typeof personJson);
 
 
 // JSON to convert Object 
 const parsePerson = JSON.parse(personJson);
-console.log(parsePerson , typeof parsePerson);
+// console.log(parsePerson , typeof parsePerson);
+
+
+
+const array = ['rohim' , 'korim' , 'sohim', 'dohim']
+console.log(array , Array.isArray(array) , typeof array);
+
+// array to json
+const arrayJson = JSON.stringify(array);
+console.log(arrayJson , typeof arrayJson);
+
+// json to array
+const arrayParseJson = JSON.parse(arrayJson);
+console.log(arrayParseJson , typeof arrayParseJson);
+
